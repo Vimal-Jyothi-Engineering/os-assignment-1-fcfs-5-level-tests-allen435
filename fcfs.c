bc8b5c679528d0c55b9e3ac5bf29535cbe3032c8
allen saji <allensaji023@gmail.com>
2026-03-03 20:25:08 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,60 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Process {
+    int pid;
+    int at;
+    int bt;
+    int wt;
+    int tat;
+    int index;  // original position
+};
+
 int main() {
     int n;
     scanf("%d", &n);
 
-    int pid[100], at[100], bt[100];
-    int wt[100], tat[100];
+    struct Process p[100];
 
     for (int i = 0; i < n; i++) {
         char pname[20];
-        scanf("%s %d %d", pname, &at[i], &bt[i]);
-        pid[i] = atoi(pname + 1);
+        scanf("%s %d %d", pname, &p[i].at, &p[i].bt);
+        p[i].pid = atoi(pname + 1);
+        p[i].index = i;  // store original position
     }
 
     // Sort by arrival time
     for (int i = 0; i < n - 1; i++)
         for (int j = 0; j < n - i - 1; j++)
-            if (at[j] > at[j + 1]) {
-                int t;
-                t=at[j];  at[j]=at[j+1];  at[j+1]=t;
-                t=bt[j];  bt[j]=bt[j+1];  bt[j+1]=t;
-                t=pid[j]; pid[j]=pid[j+1]; pid[j+1]=t;
+            if (p[j].at > p[j + 1].at) {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
 
-    // Proper FCFS calculation (ALWAYS)
     int cur = 0;
-    for (int i = 0; i < n; i++) {
-        if (cur < at[i])
-            cur = at[i];
 
-        wt[i] = cur - at[i];
-        cur += bt[i];
-        tat[i] = wt[i] + bt[i];
+    for (int i = 0; i < n; i++) {
+        if (cur < p[i].at)
+            cur = p[i].at;
+
+        p[i].wt = cur - p[i].at;
+        cur += p[i].bt;
+        p[i].tat = p[i].wt + p[i].bt;
     }
 
+    // Restore original order
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (p[j].index > p[j + 1].index) {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+
     double avgWT = 0, avgTAT = 0;
+
+    printf("Waiting Time: ");
     for (int i = 0; i < n; i++) {
-        avgWT += wt[i];
-        avgTAT += tat[i];
+        printf("P%d %d", p[i].pid, p[i].wt);
+        if (i != n - 1)
+            printf(" ");
+        avgWT += p[i].wt;
+    }
+
+    printf("\nTurnaround Time: ");
+    for (int i = 0; i < n; i++) {
+        printf("P%d %d", p[i].pid, p[i].tat);
+        if (i != n - 1)
+            printf(" ");
+        avgTAT += p[i].tat;
     }
 
     avgWT /= n;
     avgTAT /= n;
 
-    // EXACT FORMAT
-    printf("Waiting Time: ");
-    for (int i = 0; i < n; i++)
-        printf("P%d %d ", pid[i], wt[i]);
-
-    printf("\nTurnaround Time: ");
-    for (int i = 0; i < n; i++)
-        printf("P%d %d ", pid[i], tat[i]);
-
-    printf("\nAverage Waiting Time: %.2f\n", avgWT);
-    printf("Average Turnaround Time: %.2f\n", avgTAT);
+    printf("\nAverage Waiting Time: %.2f", avgWT);
+    printf("\nAverage Turnaround Time: %.2f", avgTAT);
 
     return 0;
 }
